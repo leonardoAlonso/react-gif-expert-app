9d1d1f1b117b3171bf89187941c090a98501d4a8
Leonardo Alonso <leonardoalonsososa@gmail.com>
2020-09-09 07:46:34 -0500

    Updates

---
+++ precache-manifest.86c17a065a6175df68c9448c60c13191.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27ee2b8225770d34af3fa8685b7f1254",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8971e042498e4e403e67",
+    "url": "/static/css/main.7c27699b.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8971e042498e4e403e67",
+    "url": "/static/js/main.ac66f4ae.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
