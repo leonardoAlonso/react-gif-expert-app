40f5e14c1877ad1169d11166b3ba963d9825eb22
Leonardo Alonso <leonardoalonsososa@gmail.com>
2020-09-09 07:36:30 -0500

    first commit

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,16 @@
+export const getGifs = async(category) => {
+    const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI(category) }&limit=10&api_key=UR4r5haUom4zL22mGEkdDNb3n7tNtC8E`;
+
+    const response = await fetch ( url );
+    const { data } = await response.json();
+
+    const gifs = data.map( img => {
+        return {
+            id: img.id,
+            title: img.title,
+            url: img.images?.downsized_medium.url
+        }
+    });
+
+    return gifs;
+}
