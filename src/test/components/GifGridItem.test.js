d46660d89f0297835ddf00c2e13fbcd664e047b9
Leonardo Alonso <leonardoalonsososa@gmail.com>
2020-09-15 07:56:43 -0500

    :white_check_mark: Test to validate props and clases

--- src/test/components/GifGridItem.test.js
+++ src/test/components/GifGridItem.test.js
@@ -4,17 +4,30 @@ import { GifGridItem } from '../../components/GifGridItem';
 import '@testing-library/jest-dom';
 
 describe('Debe mostrar correctamente el componente <GifGridItem>', () => {
-    // let wrapper = shallow(<GifGridItem/>)
-
-    // beforeEach(() =>{
-    //     wrapper = shallow(<GifGridItem/>)
-    // })
-
     const title = 'Soy un titulo';
     const url = 'https://localhost/algo/image.jpg';
 
+    const wrapper = shallow(<GifGridItem title={title} url={url}/>)
+
     test('GifGridItem default', () => {
-        const wrapper = shallow( <GifGridItem title={title} url={url}/> );
         expect(wrapper).toMatchSnapshot();
+    });
+
+
+    test('Debe tener un parrafo con el titulo', () => {
+        const p = wrapper.find('p');
+        expect(p.text().trim()).toBe(title);
+    });
+
+    test('Debe tener la imagen igual a los props', () => {
+        const img = wrapper.find('img');
+        expect(img.prop('src')).toBe(url);
+        expect(img.prop('alt')).toBe(title);
+    });
+
+    test('Debe tener animate__fadeIn', () => {
+        const div = wrapper.find('div');
+        const classes = div.prop('className');
+        expect(classes.includes('animate__fadeIn')).toBe(true);
     })
 })
