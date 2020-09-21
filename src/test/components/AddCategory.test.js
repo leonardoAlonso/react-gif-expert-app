e78aea9f9655031cf64252ffbdf327531ae0b4ee
Leonardo Alonso <leonardoalonsososa@gmail.com>
2020-09-21 07:53:42 -0500

    :white_check_mark: Simulate onChange and onSumbit

--- src/test/components/AddCategory.test.js
+++ src/test/components/AddCategory.test.js
@@ -1,15 +1,43 @@
 import React from 'react';
 import {shallow} from 'enzyme';
 import { AddCategory } from '../../components/AddCategory'
+import '@testing-library/jest-dom'
 
 
 describe('Pruebas en el componente <AddCategory/>', () => {
 
-    const setCategories = () => {}
+    const setCategories = jest.fn();
+    let wrapper = shallow( <AddCategory setCategories={setCategories}/> );
+
+    beforeEach( () => {
+        jest.clearAllMocks();
+        wrapper = shallow( <AddCategory setCategories={setCategories}/> );
+    });
 
 
     test('debe de mostrarse correctamente', () => {
-        const wrapper = shallow( <AddCategory setCategories={setCategories}/> );
         expect( wrapper ).toMatchSnapshot();
-    })
-})
+    });
+
+    test('debe de cambiar la caja de texto', () => {
+        const input = wrapper.find('input');
+        const value = 'Hola mundo';
+        input.simulate('change', { target: { value: value } });
+    });
+
+    test('no debe de postear la informacion', () => {
+        wrapper.find('form').simulate('submit', { preventDefault(){} });
+        expect(setCategories).not.toHaveBeenCalled();
+    });
+
+    test('debe de llamar el set categories y limpiar la caja de texto', () => {
+        let input = wrapper.find('input');
+        const value = 'Goku';
+        input.simulate('change', { target: { value: value } });
+        wrapper.find('form').simulate('submit', { preventDefault(){} });
+        expect(setCategories).toHaveBeenCalled();
+        expect(setCategories).toHaveBeenCalledTimes(1);
+        expect(setCategories).toHaveBeenCalledWith( expect.any(Function) );
+        expect(input.prop('value')).toBe('');
+    });
+});
